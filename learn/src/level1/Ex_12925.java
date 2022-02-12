7cda1f800c1eea651391d98029e529b68b9f389d
Daehyeon Kim <eogus8292@naver.com>
2022-02-12 14:33:42 +0900

    문자열을 정수로 바꾸기

---
+++ learn/src/level1/Ex_12925.java
@@ -0,0 +1,18 @@
+package level1;
+
+/*
+ 문자열을 정수로 바꾸기
+ 
+ < 문제 >
+ 
+ 문자열 s를 숫자로 변환한 결과를 반환하는 함수, solution을 완성하세요.
+ */
+
+public class Ex_12925 {
+	
+    public int solution(String s) {
+        int answer = Integer.parseInt(s);
+
+        return answer;
+    }
+}
