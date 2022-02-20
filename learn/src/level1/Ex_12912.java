26e173a2134670874d32daabbf38f9677998d44b
Daehyeon Kim <eogus8292@naver.com>
2022-02-20 16:06:44 +0900

    두 정수 사이의 합

---
+++ learn/src/level1/Ex_12912.java
@@ -0,0 +1,26 @@
+package level1;
+
+/*
+
+< 문제 >
+두 정수 a, b가 주어졌을 때 a와 b 사이에 속한 모든 정수의 합을 리턴하는 함수, solution을 완성하세요.
+예를 들어 a = 3, b = 5인 경우, 3 + 4 + 5 = 12이므로 12를 리턴합니다. 
+*/
+
+public class Ex_12912 {
+
+	public long solution(int a, int b) {
+        long answer = 0;
+        if(a>b){
+            for(int i = b; i <=a;  i++){
+                answer += i;
+            }
+        }else{
+            for(int i = a; i <=b ;  i++){
+                answer += i;
+            }
+        }
+        return answer;
+    }
+
+}
