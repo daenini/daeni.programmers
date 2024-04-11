9b2113662c7518a2cce62927d04c86361579d35b
Daeni <82662325+daenini@users.noreply.github.com>
2024-04-11 20:04:44 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        String answer = "Yes";
+        
+        int cards1Index = 0;
+        int cards2Index = 0;
+        
+        
+        for (String goalStr : goal ) {
+            if (cards1Index < cards1.length && goalStr.equals(cards1[cards1Index])) {
+                cards1Index++;
+            } else if (cards2Index < cards2.length && goalStr.equals(cards2[cards2Index])) {
+                cards2Index++;
+            } else {
+                answer = "No";
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
