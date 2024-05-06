1912aee411498e3d2ead4efb8952611ea38ff8cb
Daeni <82662325+daenini@users.noreply.github.com>
2024-05-06 22:25:21 +0900

    [level 0] Title: 5명씩, Time: 0.02 ms, Memory: 83.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String[] solution(String[] names) {
+        
+        int size = names.length % 5 == 0 ? names.length / 5 : names.length / 5 + 1;        
+        String[] answer = new String[size];
+        
+        int index = 0;
+        for( int i=0; i<names.length; i+=5) {
+        	answer[index] = names[i];
+        	index++;
+        }
+        
+        
+        return answer;
+    }
+}
